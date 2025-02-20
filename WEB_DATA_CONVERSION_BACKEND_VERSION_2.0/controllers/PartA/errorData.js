c1e48205e51c9207b8ce0f98a78c9169136de55b
Abhishek kumar <abhi.arya97161@gmail.com>
2025-02-20 14:40:52 +0530

    Fix variable naming in error data retrieval to improve clarity

--- WEB_DATA_CONVERSION_BACKEND_VERSION_2.0/controllers/PartA/errorData.js
+++ WEB_DATA_CONVERSION_BACKEND_VERSION_2.0/controllers/PartA/errorData.js
@@ -4,13 +4,13 @@ const errorData = async (req, res) => {
   const assignId = req.params.id;  
 
   try {
-    const data = await AssignData.findOne({ where: { id: assignId } }); // Corrected query
+    const assigndata = await AssignData.findOne({ where: { id: assignId } }); // Corrected query
 
-    if (!data) {
+    if (!assigndata) {
       return res.status(404).json({ error: "Data not found" }); // Handle case where no data is found
     }
 
-    return res.status(200).json({ data }); // Send the found data
+    return res.status(200).json({ assigndata }); // Send the found data
   } catch (error) {
     console.error("Error fetching data:", error);
     return res.status(500).json({ error: "Internal server error" });
